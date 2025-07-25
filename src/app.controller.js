e3b95314b54f88c4b4cfd21d0e999e94643f066d
mahmoud09sakr <mahmoudroute199@gmail.com>
2025-07-25 19:53:13 +0300

    add log

--- src/app.controller.js
+++ src/app.controller.js
@@ -7,6 +7,8 @@ export const bootstrap = (app, express) => {
     app.use(express.json())
     databaseConnection()
     console.log("im in auth branch ");
+    console.log("im in auth branch ");
+
 
 
     app.use('/api/v1/users', userRouter)
